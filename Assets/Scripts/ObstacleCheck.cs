1c29aa49106a79e82c163e288f11ace05bb85a8c
jj139811 <jj139811@kaist.ac.kr>
2017-11-23 20:32:26 +0900

    Move obstacles
    We can now move obstacles on the game play display
    Hold an obstacle and drag it to where you want

--- Assets/Scripts/ObstacleCheck.cs
+++ Assets/Scripts/ObstacleCheck.cs
@@ -6,11 +6,15 @@ public class ObstacleCheck : MonoBehaviour {
     private GameObject sceneManager;
     private SceneManager m;
     private Vector3 pre, cur;
+    private Vector3 displacement;
+    public bool selected;
+
 	// Use this for initialization
 	void Start () {
         pre = transform.position;
         sceneManager = GameObject.Find("SceneManager");
         m = sceneManager.GetComponent<SceneManager>();
+        selected = false;
 	}
 	
 	// Update is called once per frame
@@ -21,5 +25,20 @@ public class ObstacleCheck : MonoBehaviour {
             m.ReplanningFlag = true;
         }
         pre = cur;
+        if (!Input.GetButton("Fire1"))
+        {
+            selected = false;
+        }
+        if (selected)
+        {
+            Vector3 temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(temp.x + displacement.x, 0, temp.z + displacement.z);
+        }
 	}
+
+    void OnMouseDown()
+    {
+        selected = true;
+        displacement = - Camera.main.ScreenToWorldPoint(Input.mousePosition) + transform.position;
+    }
 }
